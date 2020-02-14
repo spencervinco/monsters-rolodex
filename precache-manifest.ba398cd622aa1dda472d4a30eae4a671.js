1e0b2883cf1aac7884ea8339e2484de4c8268bd6
Spencer Vinco <spencer_vinco@anfcorp.com>
2020-02-14 10:56:43 -0500

    Updates

---
+++ precache-manifest.ba398cd622aa1dda472d4a30eae4a671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676319b647b26e66df8cbf2443282f21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d6a48747429ab6b71d3",
+    "url": "/monsters-rolodex/static/css/main.23d4eef0.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6a48747429ab6b71d3",
+    "url": "/monsters-rolodex/static/js/main.6d6ed080.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
